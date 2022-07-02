d91d3d0f4236b5213258cb1bd3c352b5c6e597bf
Joshua Manchester <joshuamanchester4@gmail.com>
2022-07-03 00:01:43 +0100

    Updated wsl install

--- .config/installers/wsl_install.sh
+++ .config/installers/wsl_install.sh
@@ -7,14 +7,24 @@ then
     exit
 fi
 
+# update and upgrade
+apt update && apt upgrade -y
+
+# create sudo user
+echo "Enter your username:"
+read username
+useradd $username
+useradd $username sudo
+su $username
+whoami
+
 # clone and checkout dot files repo
 git clone --bare https://github.com/JManch/dotfiles $HOME/.dotfiles
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME checkout
 git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME config --local status.showUntrackedFiles no
 
 # install and configure zsh
-apt-get install zsh
+apt-get install zsh -y
 echo "export ZDOTDIR=~/.config/zsh" >> /etc/zsh/zshenv 
 chsh -s $(which zsh)
-~/.config/zsh/plugins/plugin_installer.sh
 
